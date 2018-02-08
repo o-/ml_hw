c2cafa01df6a8db911ed078e17a7cdbff22d2355
o <o@o1o.ch>
2018-02-08 12:16:50 +0100

    6c solutions

--- 2/gen6c.sh
+++ 2/gen6c.sh
@@ -4,6 +4,7 @@
 ridge="-1 1 0.01"
 
 echo "--== Searching for good ridge regression parameters between -1 and 1 ==--"
+echo
 for n in 2 3 5; do
   echo "****** n = $n"
   for k in 2 10 100; do
@@ -15,3 +16,18 @@ for n in 2 3 5; do
     echo
   done
 done
+
+echo
+echo "--== How does the test error change with different n ==--"
+echo "      A: n == 3 fits best"
+echo
+./lr.py -i dataset2.mat --poly 2 3 5 --gd --err
+
+echo
+echo "--== How does the test error change with λ ==--"
+echo "    A: there seems to be a globally optimal value"
+echo
+echo "****** n = 3"
+./lr.py -i dataset2.mat --poly 3 --ridge-seq $ridge --gd --err
+echo "****** n = 5"
+./lr.py -i dataset2.mat --poly 5 --ridge-seq $ridge --gd --err
